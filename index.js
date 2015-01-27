bf2b24919363cb3cf54309b100d51327ace4e761
Ron Gruesbeck <rongruesbeck@gmail.com>
2015-01-27 14:19:30 -0800

    commit

--- index.js
+++ index.js
@@ -17,8 +17,15 @@ var sock = shoe(function(stream) {
   stream.pipe(multiStream()).pipe(stream);
 });
 
+function newPeer(sid){
+  return {
+    id: 'peer!' + Math.random().toString(16).slice(2),
+    socketId: sid
+  };
+}
+
 function multiStream(){
-  console.log('multistream function fired');
+  var peer;
   var mdm = MuxDemux();
   mdm.on('connection', connectHandler);
   mdm.on('error', function(err){
@@ -26,9 +33,45 @@ function multiStream(){
     mdm.destroy();
   });
 
+  mdm.once('connection', function(stream){
+    peer = newPeer(stream.id);
+    if (!sock.peersockets) {
+      sock.peersockets = [];
+    }
+
+    db.put(peer.id, JSON.stringify(peer), function(err){
+      if (err) return console.log(err);
+      sock.peersockets.push(stream);
+      console.log(peer.id + ' connected.');
+    });
+
+    stream.on('close', function(){
+      db.del(peer.id, function(err){
+        if (err) return console.log(err);
+        var tmp = null;
+        for (i=0; i<sock.peersockets.length; ++i) {
+          if (sock.peersockets[i] === peer.socketId) {
+            tmp = i;
+            break;
+          }
+        }
+        sock.peersockets.splice(tmp, 1);
+        console.log(peer.id + ' disconected.');
+      });
+    });
+
+  });
+
   function connectHandler(stream){
-    console.log('connect handler fired');
-    console.log(stream.meta);
+
+    if (stream.meta === 'rpc') {
+      console.log('stream is rpc');
+      //var signaler = require('./modules/signaler')(peer, db);
+      //var rpc = rpcStream(signaler);
+      //rpc.pipe(stream).pipe(rpc);
+    } else {
+      console.log('stream is notice');
+    }
   }
 
   return mdm;
